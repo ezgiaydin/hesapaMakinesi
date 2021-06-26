e54ed4e96e4a5afbf1d8b6f03d16691399727975
Ezgi Aydın <83668857+ezgiaydin@users.noreply.github.com>
2021-06-27 00:20:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace hesapmak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            double answer;
+            string process;
+
+            Console.WriteLine("Enter first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an operator(*,/,+,-)");
+            process = Console.ReadLine();
+
+            if (process == "+")
+            {
+                answer = num1 + num2;
+                Console.WriteLine("Answer: " + answer);
+            }
+            if (process == "-")
+            {
+                answer = num1 - num2;
+                Console.WriteLine("Answer:" + answer);
+            }
+            if (process == "*")
+            {
+                answer = num1 * num2;
+                Console.WriteLine("Answer: " + answer);
+            }
+            if (process == "/")
+            {
+                answer = num1 / num2;
+                Console.WriteLine("Answer: " + answer);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect operator entry!");
+            }
+
+
+
+        }
+    }
+}
